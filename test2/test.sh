8f753c280e70d187451c4bf4fbedfd0e83b28e28
Angel Xie <angel.xie4956@gmail.com>
2024-05-05 21:45:47 +0800

    adding print

--- test2/test.sh
+++ test2/test.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
+
+# Compile the client and server programs
 gcc -o test_client client.c
 gcc -o test_server server.c
 
+# Copy the compiled client executable to different directories
 cp test_client ./client1/
 cp test_client ./client2/
 
-./test_server > /dev/null 2>&1 &
+# Start the server in the background, but allow console output
+./test_server &
 
+# Navigate to client2 directory and execute client with input redirection from input.txt
 cd ./client2/
-./test_client < input.txt > /dev/null 2>&1 &
+./test_client < input.txt &
 cd ..
 
+# Navigate to client1 directory and execute client with input redirection from input.txt
 cd ./client1/
-./test_client < input.txt > /dev/null 2>&1 &
+./test_client < input.txt &
 cd ..
 
+# Wait for 10 seconds to allow processes to complete
 sleep 10
 
+# Check if files are identical between the two clients
 diff ./client1/20.txt ./client2/20.txt
 
+# Check the exit status of the last command (diff)
 if [ $? -eq 0 ]; then
     echo "Succeed"
 else
     echo "Fail"
 fi
 
+# Kill the test_client and test_server processes
 pkill test_client
 pkill test_server
+
+# Remove specific file
 rm ./client1/20.txt
